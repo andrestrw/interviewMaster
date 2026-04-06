6a116b2e312675a9c60af99509e573ce31399ed2
Andres Chicaiza <achicaizad@doomity.com>
2026-04-06 17:55:17 +0200

    feat(auth): integrate react-hook-form and zod validation into login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,28 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema, type LoginData } from "@/schemas/auth";
+
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginData>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginData) => {
+    try {
+    } catch (error) {}
+  };
+
   return (
     <div className="flex items-center justify-center min-h-dvh">
       <div className="w-full max-w-sm px-4">
@@ -24,14 +45,24 @@ export default function Login() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form action="#" method="post" className="space-y-4">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="space-y-4"
+              noValidate
+            >
               <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   type="email"
                   id="email"
                   placeholder="ephraim@blocks.so"
+                  {...register("email")}
                 />
+                {errors.email && (
+                  <p className="text-xs font-medium text-destructive">
+                    {errors.email.message}
+                  </p>
+                )}
               </div>
               <div className="space-y-2">
                 <Label htmlFor="password">Password</Label>
@@ -39,10 +70,16 @@ export default function Login() {
                   type="password"
                   id="password"
                   placeholder="**************"
+                  {...register("password")}
                 />
+                {errors.password && (
+                  <p className="text-xs font-medium text-destructive">
+                    {errors.password.message}
+                  </p>
+                )}
               </div>
-              <Button type="submit" className="w-full">
-                Sign in
+              <Button type="submit" disabled={isSubmitting} className="w-full">
+                {isSubmitting ? "Signing in..." : "Sign in"}
               </Button>
             </form>
 
